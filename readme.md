a85fe54db466469f56e2e25fb9057646c2559032
sergiuchilat <chilatsergiu@gmail.com>
2023-02-18 16:43:12 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,92 @@
+1. Run nmp install
+2. run nodemon src/index.js
+
+# API documentation
+
+Available statuses:
+- available - user is online and available 
+- busy - user is online but is busy
+- away - user is away
+
+### Ping user activity
+
+Request method: POST
+
+Request URL ``/users/ping?key=env.ACCESS_KEY``
+
+Request body:
+```
+{
+    "user": UUID,
+    "status": string,
+    "time": datetime
+}
+```
+
+Response:
+```
+{
+    "data": {
+        "user": "3",
+        "status": "busy",
+        "time": "2023-02-18 16:33:13"
+    }
+}
+```
+
+### Get list of users in a specified status
+
+Request method: GET
+
+Request URL ``/users/list/{status}?key=env.ACCESS_KEY``
+
+> status = all: will return all useers
+
+Request body:
+```
+NULL
+```
+
+Response:
+```
+{
+    "data": [
+        {
+            "user": "1",
+            "status": "busy",
+            "time": "2023-02-18 16:40:29"
+        },
+        {
+            "user": "2",
+            "status": "busy",
+            "time": "2023-02-18 16:40:27"
+        },
+        {
+            "user": "3",
+            "status": "busy",
+            "time": "2023-02-18 16:40:24"
+        }
+    ]
+}
+```
+
+
+
+### Flush database - will clear all data
+
+Request method: GET
+Request URL ``/database/clear?key=env.ACCESS_KEY_SYSTEM``
+
+> status = all: will return all useers
+
+Request body:
+```
+NULL
+```
+
+Response:
+```
+{
+    "data": "success"
+}
+```
---
+++ src/index.js
@@ -0,0 +1,120 @@
+const express = require('express');
+require('dotenv').config()
+const app = express();
+app.use(express.json());
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const { InMemoryDatabase } = require('in-memory-database');
+
+const database = new InMemoryDatabase();
+
+const accessGranted = (key, level = '') => {
+    if(level === 'SYSTEM'){
+        return process.env.ACCESS_KEY_SYSTEM === key;
+    }
+    return process.env.ACCESS_KEY === key;
+    
+}
+
+const getUsers = (status = 'all') => {
+    const keys = database.keys();
+    const users = [];
+    keys.forEach(key => {
+        const user = {...database.get(key)};
+
+        if(status === 'all' || user.status === status){
+            users.push(user)
+        }
+    })
+
+    return users;
+}
+
+const isSupportedStatus = (status) => ['all', 'available', 'busy', 'away'].includes(status);
+
+app.post('/users/ping', (request, response) => {
+    if(!accessGranted(request.query.key)){
+        response.send({'error': 'access-denied'})
+        return;
+    }
+    try{
+        const status = request.body.status;
+        if(!isSupportedStatus(status)){
+            response.send({'error': 'invalid-status'});
+            return;
+        }
+        database.set(request.body.user, request.body);
+        response.send({
+            data: database.get(request.body.user)
+        })
+    } catch(e){
+        response.send({
+            error: e
+        });
+    }
+})
+
+app.get('/users/list/:status', (request, response) => {
+    if(!accessGranted(request.query.key)){
+        response.send({'error': 'access-denied'})
+        return;
+    }
+    try{   
+        const status = request.params.status;
+        if(!isSupportedStatus(status)){
+            response.send({'error': 'invalid-status'})
+            return;
+        }
+        response.send({
+            data: getUsers(status)
+        })
+    }catch(e){
+        response.send({
+            errer: e
+        });
+    }
+})
+
+app.get('/users/count/:status', (request, response) => {
+    if(!accessGranted(request.query.key)){
+        response.send({'error': 'access-denied'})
+        return;
+    }
+    try{   
+        const status = request.params.status;
+        if(!isSupportedStatus(status)){
+            response.send({'error': 'invalid-status'});
+            return;
+        }
+        response.send({
+            data: getUsers(status).length
+        })
+    }catch(e){
+        response.send({
+            error: e
+        });
+    }
+})
+
+app.get('/database/clear', (request, response) => {
+    if(!accessGranted(request.query.key, 'SYSTEM')){
+        response.send({'error': 'access-denied'})
+        return;
+    }
+    try{   
+        database.flush();
+        response.send({
+            data: 'success'
+        })
+    }catch(e){
+        response.send({
+            error: e
+        });
+    }
+})
+
+app.listen(process.env.SERVER_PORT, process.env.SERVER_URL, () => {
+    console.log(`Example app listening on port ${process.env.SERVER_PORT}`)
+})
